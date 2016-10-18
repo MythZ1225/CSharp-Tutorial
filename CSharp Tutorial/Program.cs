717c4941affe784c67ec37f368908d3ba6832782
MythZ <zen1225@gmail.com>
2016-10-19 02:02:38 +0800

    [MZ]Add Guess Number practice

--- CSharp Tutorial/Program.cs
+++ CSharp Tutorial/Program.cs
@@ -10,6 +10,33 @@ namespace CSharp_Tutorial
     {
         static void Main(string[] args)
         {
+            string answer = "4"; // 電腦設定
+        
+
+            Console.WriteLine("Want play a game?");
+            Console.WriteLine("a num between 1~10?");
+            Console.WriteLine("what is it?");
+
+            bool userWasNotRight = true;
+
+            while (userWasNotRight) {
+                String guess = Console.ReadLine();
+
+                Console.WriteLine("your guess is " + guess);
+               
+                if (answer == guess)
+                {
+                    Console.WriteLine("You R correct");
+                    userWasNotRight = false;
+                }
+                else
+                {
+                    Console.WriteLine("Missed! Try again!");
+                }
+            }
+
+            
+
         }
     }
 }
